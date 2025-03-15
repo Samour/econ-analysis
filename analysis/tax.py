6f2d3131a9b9bec49583df83e498bea41d8167c5
Aaron Burke <aaronb@canva.com>
2025-03-15 18:48:57 +1000

    Make a start with python code

---
+++ analysis/tax.py
@@ -0,0 +1,24 @@
+from dataclasses import dataclass
+
+
+@dataclass(frozen=True)
+class TaxBracket:
+    start: int
+    end: int
+    # TODO Change these to correct decimal type
+    rate: float
+
+
+@dataclass(frozen=True)
+class TaxTable:
+    year: int
+    brackets: list[TaxBracket]
+
+
+@dataclass(frozen=True)
+class MultiYearTaxTable:
+    year_tables: list[TaxTable]
+
+
+def load_tax_tables(fname: str) -> None:
+    pass
